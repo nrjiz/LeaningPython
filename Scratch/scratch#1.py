6ca408bb83a493a73f90fed5a930fd6cdb6f8ed3
nrjiz <164118742+nrjiz@users.noreply.github.com>
2024-07-30 15:40:00 +0300

    Update and rename scratch#1 to scratch#1.py

--- Scratch/scratch#1.py
+++ Scratch/scratch#1.py
@@ -7,32 +7,29 @@ dict = {
     
 }
 
-
-for key in dict.keys():
-    print(type(key),type(dict[key]),sep="--->>>")
-    
-for values in dict.values():
-    print(values)
-    
-
-dict = {
-    'meow':[1,2,3],
-    "moo":["sound of cows","lavash kiri"],
-    (1,2):"amazing tuple",
-    69: "an intresting number",
-    6.9:('byekhud float?',),
-    
-}
-
 for key in dict.keys():
     print(type(key),type(dict[key]),sep="--->>>")
 for key,values in dict.items():
     print(values,"is image of ",key)
+
+dict2 = {
+    1:[1,2,3],
+    2:[4,5,6],
+    3:[7,8,9]
+    }
     
+print(dict2[1].append(8))    
+print(dict2[1])
+a = 4
+if(a not in dict2):
+    dict2[a]=[3]
+print(dict2)
+for key, value in dict2.items():
+    print(key,"---> ",value)
+
 
     
     
 
     
-
 
